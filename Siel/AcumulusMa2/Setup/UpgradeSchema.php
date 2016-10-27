5d48b0cdda2a57d24d4ee69731c6f3bfe26b6737
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2016-10-27 14:21:16 +0200

    MA2.1 testen

--- Siel/AcumulusMa2/Setup/UpgradeSchema.php
+++ Siel/AcumulusMa2/Setup/UpgradeSchema.php
@@ -16,10 +16,9 @@ class UpgradeSchema implements UpgradeSchemaInterface
         if($context->getVersion()) {
             // Previous version found, this is an upgrade, not an installation.
             /** @var \Siel\AcumulusMa2\Helper\Data $helper */
-            $helper = Registry::getInstance()->get('\Siel\AcumulusMa2\Helper\Data');
+            $helper = Registry::getInstance()->get('Siel\AcumulusMa2\Helper\Data');
             $helper->getAcumulusConfig()->upgrade($context->getVersion());
         }
-
         $setup->endSetup();
     }
 }
