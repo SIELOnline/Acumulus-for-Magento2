af8125bb433837606ef28618b8005a19b89645e9
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2017-09-30 15:16:15 +0200

    lib v5: "require" it via composer, remove own autoloader statements from registration.php

--- Siel/AcumulusMa2/registration.php
+++ Siel/AcumulusMa2/registration.php
@@ -6,10 +6,3 @@
 use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Siel_AcumulusMa2', __DIR__);
-
-// See http://magento.stackexchange.com/a/114966/38943.
-$vendorDir = require BP . '/app/etc/vendor_path.php';
-$vendorAutoload = BP . "/{$vendorDir}/autoload.php";
-/** @var \Composer\Autoload\ClassLoader $composerAutoloader */
-$composerAutoloader = include $vendorAutoload;
-$composerAutoloader->addPsr4('Siel\\Acumulus\\', array(__DIR__ . '/src/Siel/Acumulus'));
--- copy-ma2-to-dev.bat
+++ copy-ma2-to-dev.bat
@@ -1,3 +1,5 @@
 @echo off
 rem Copy files in our folder structure to development installation.
 mklink /J D:\Projecten\Acumulus\Magento\www21\app\code\Siel D:\Projecten\Acumulus\Webkoppelingen\Magento21\Siel
+mkdir D:\Projecten\Acumulus\Magento\www21\vendor\siel
+mklink /J D:\Projecten\Acumulus\Magento\www21\vendor\siel\acumulus D:\Projecten\Acumulus\Webkoppelingen\libAcumulus
