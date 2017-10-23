04cfa3a4c9edbdfe89545bc0d38ab6e89f1fa979
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2017-10-23 17:06:44 +0200

    Batch files aangepast aan nieuwe struktuur.

--- copy-ma2-to-dev22.bat
+++ copy-ma2-to-dev22.bat
@@ -1,6 +1,5 @@
 @echo off
 rem Copy files in our folder structure to development installation.
-mkdir D:\Projecten\Acumulus\Magento\www22\app\code\siel
-mklink /J D:\Projecten\Acumulus\Magento\www22\app\code\siel\acumulus D:\Projecten\Acumulus\Webkoppelingen\Magento21\siel_acumulus
 mkdir D:\Projecten\Acumulus\Magento\www22\vendor\siel
 mklink /J D:\Projecten\Acumulus\Magento\www22\vendor\siel\acumulus D:\Projecten\Acumulus\Webkoppelingen\libAcumulus
+mklink /J D:\Projecten\Acumulus\Magento\www22\vendor\siel\acumulus-ma2 D:\Projecten\Acumulus\Webkoppelingen\Magento21\siel_acumulus
