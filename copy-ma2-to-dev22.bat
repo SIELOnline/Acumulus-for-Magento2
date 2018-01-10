164b34db42351d29e4968fbf6407b2dd006b1ea1
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2018-01-10 09:53:57 +0100

    MA2 on library v5 revisited

--- copy-ma2-to-dev22.bat
+++ copy-ma2-to-dev22.bat
@@ -1,6 +1,10 @@
 @echo off
 rem Copy files in our folder structure to development installation.
-mkdir D:\Projecten\Acumulus\Magento\www22\vendor\siel
+mkdir D:\Projecten\Acumulus\Magento\www22\vendor\siel 2> nul
+rmdir /s /q D:\Projecten\Acumulus\Magento\www22\vendor\siel\acumulus 2> nul
 mklink /J D:\Projecten\Acumulus\Magento\www22\vendor\siel\acumulus D:\Projecten\Acumulus\Webkoppelingen\libAcumulus
-mklink /J D:\Projecten\Acumulus\Magento\www22\vendor\siel\acumulus-ma2 D:\Projecten\Acumulus\Webkoppelingen\Magento21\acumulus-ma2
-mklink /J D:\Projecten\Acumulus\Magento\www21\app\code\Siel D:\Projecten\Acumulus\Webkoppelingen\Magento21\Siel
+rmdir /s /q D:\Projecten\Acumulus\Magento\www22\vendor\siel\acumulus-ma2 2> nul
+mklink /J D:\Projecten\Acumulus\Magento\www22\vendor\siel\acumulus-ma2 D:\Projecten\Acumulus\Webkoppelingen\Magento2\siel-acumulus-ma2
+mkdir D:\Projecten\Acumulus\Magento\www22\app\code 2> nul
+rmdir /s /q D:\Projecten\Acumulus\Magento\www22\app\code\Siel 2> nul
+mklink /J D:\Projecten\Acumulus\Magento\www22\app\code\Siel D:\Projecten\Acumulus\Webkoppelingen\Magento2\Siel
--- copy-shared-to-ma2.bat
+++ copy-shared-to-ma2.bat
@@ -1,9 +1,14 @@
 @echo off
 rem Link license files to here.
-mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento21\acumulus-ma2\license.txt     D:\Projecten\Acumulus\Webkoppelingen\libAcumulus\license.txt
-mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento21\acumulus-ma2\licentie-nl.pdf D:\Projecten\Acumulus\Webkoppelingen\libAcumulus\licentie-nl.pdf
-mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento21\acumulus-ma2\leesmij.txt     D:\Projecten\Acumulus\Webkoppelingen\leesmij.txt
+del D:\Projecten\Acumulus\Webkoppelingen\Magento2\siel-acumulus-ma2\license.txt 2> nul
+mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento2\siel-acumulus-ma2\license.txt D:\Projecten\Acumulus\Webkoppelingen\libAcumulus\license.txt
+del D:\Projecten\Acumulus\Webkoppelingen\Magento2\siel-acumulus-ma2\licentie-nl.pdf 2> nul
+mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento2\siel-acumulus-ma2\licentie-nl.pdf D:\Projecten\Acumulus\Webkoppelingen\libAcumulus\licentie-nl.pdf
+del D:\Projecten\Acumulus\Webkoppelingen\Magento2\siel-acumulus-ma2\leesmij.txt 2> nul
+mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento2\siel-acumulus-ma2\leesmij.txt D:\Projecten\Acumulus\Webkoppelingen\leesmij.txt
 
 rem Link license files to example module.
-mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento21\Siel\AcumulusCustomiseInvoice\license.txt     D:\Projecten\Acumulus\Webkoppelingen\libAcumulus\license.txt
-mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento21\Siel\AcumulusCustomiseInvoice\licentie-nl.pdf D:\Projecten\Acumulus\Webkoppelingen\libAcumulus\licentie-nl.pdf
+del D:\Projecten\Acumulus\Webkoppelingen\Magento2\Siel\AcumulusCustomiseInvoice\license.txt 2> nul
+mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento2\Siel\AcumulusCustomiseInvoice\license.txt D:\Projecten\Acumulus\Webkoppelingen\libAcumulus\license.txt
+del D:\Projecten\Acumulus\Webkoppelingen\Magento2\Siel\AcumulusCustomiseInvoice\licentie-nl.pdf 2> nul
+mklink /H D:\Projecten\Acumulus\Webkoppelingen\Magento2\Siel\AcumulusCustomiseInvoice\licentie-nl.pdf D:\Projecten\Acumulus\Webkoppelingen\libAcumulus\licentie-nl.pdf
