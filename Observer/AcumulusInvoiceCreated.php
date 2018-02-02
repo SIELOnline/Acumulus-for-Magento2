e6ad63a728134c5a7764a06641eb387f09c80d20
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2018-02-02 16:18:28 +0100

    5.0.6; Add support for payment fees by sisow module

--- Observer/AcumulusInvoiceCreated.php
+++ Observer/AcumulusInvoiceCreated.php
@@ -1,8 +1,11 @@
 <?php
 namespace Siel\AcumulusMa2\Observer;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Tax\Model\Calculation;
 use Siel\Acumulus\Invoice\Creator;
 use Siel\Acumulus\Invoice\Source;
 use Siel\Acumulus\Meta;
@@ -16,6 +19,16 @@ class AcumulusInvoiceCreated implements ObserverInterface
 {
 
     /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * @var \Magento\Tax\Model\Calculation
+     */
+    protected $taxCalculation;
+
+    /**
      * @var \Siel\AcumulusMa2\Helper\Data
      */
     private $helper;
@@ -23,10 +36,14 @@ class AcumulusInvoiceCreated implements ObserverInterface
     /**
      * SalesOrderSaveAfter constructor.
      *
-     * @param Data $helper
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Tax\Model\Calculation $taxCalculation
+     * @param \Siel\AcumulusMa2\Helper\Data $helper
      */
-    public function __construct(Data $helper)
+    public function __construct(ScopeConfigInterface $scopeConfig, Calculation $taxCalculation, Data $helper)
     {
+        $this->scopeConfig = $scopeConfig;
+        $this->taxCalculation = $taxCalculation;
         $this->helper = $helper;
     }
 
@@ -47,6 +64,8 @@ class AcumulusInvoiceCreated implements ObserverInterface
      * @param \Magento\Framework\Event\Observer $observer
      *
      * @return void
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function execute(Observer $observer)
     {
@@ -56,6 +75,7 @@ class AcumulusInvoiceCreated implements ObserverInterface
         $invoiceSource = $observer->getDataByKey('source');
 
         $this->supportPaycheckout($invoice, $invoiceSource);
+        $this->supportSisow($invoice, $invoiceSource);
 
         // Pass changes back to Acumulus.
         $observer->setData('invoice', $invoice);
@@ -100,6 +120,67 @@ class AcumulusInvoiceCreated implements ObserverInterface
             // @see \Siel\Acumulus\Magento\Invoice\Creator\getInvoiceTotals()
             $invoice['customer']['invoice'][Meta::InvoiceAmountInc] += $paymentInc;
             $invoice['customer']['invoice'][Meta::InvoiceVatAmount] += $paymentVat;
+        }
+    }
+
+    /**
+     * Adds support for the sisow module.
+     *
+     * The sisow module allows to define a payment fee per payment method. If a
+     * fee has been defined and applied to an order it is stored in the Payment
+     * object.
+     *
+     * Unfortunately:
+     * - No tax info is stored with that, so we retrieve the current tax rate
+     *   and hope it was the same at the time the order for which an invoice is
+     *   now being created was ordered.
+     * - There's no payment object on credit memos, so we can't support that.
+     *
+     * @see https://www.sisow.nl/implementatie-plugin
+     *
+     * @param array $invoice
+     * @param \Siel\Acumulus\Invoice\Source $invoiceSource
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    protected function supportSisow(array &$invoice, Source $invoiceSource)
+    {
+        if ($invoiceSource->getType() === Source::Order) {
+            /** @var \Magento\Sales\Model\Order $order */
+            $order = $invoiceSource->getSource();
+            $payment = $order->getPayment();
+            $additionalInfo = $payment->getAdditionalInformation();
+            if (array_key_exists('sisow', $additionalInfo) && (float) $additionalInfo['sisow'] != 0.0) {
+                $paymentEx = (float) $additionalInfo['sisow'];
+
+                // Get vat.
+                // @todo: $order->getCustomerClassId() can that be set?
+                /** @noinspection PhpUndefinedMethodInspection */
+                $request = $this->taxCalculation->getRateRequest($order->getShippingAddress(), $order->getBillingAddress(), $order->getCustomerClassId(), null, $order->getCustomerId());
+                /** @noinspection PhpUndefinedMethodInspection */
+                $request->setProductClassId($this->scopeConfig->getValue('sisow/general/feetaxclass', ScopeInterface::SCOPE_STORE));
+                $paymentVatRate = $this->taxCalculation->getRate($request);
+                $paymentVat = $paymentEx * ($paymentVatRate / 100.0);
+
+                $paymentInc = $paymentEx + $paymentVat;
+                $line = [
+                    Tag::Product => $this->helper->t('payment_costs'),
+                    Tag::Quantity => 1,
+                    Tag::UnitPrice => $paymentEx,
+                    Meta::UnitPriceInc => $paymentInc,
+                    Tag::VatRate => $paymentVatRate,
+                    Meta::VatRateSource => Creator::VatRateSource_Exact,
+                    Meta::VatAmount => $paymentVat,
+                    Meta::FieldsCalculated => [Meta::UnitPriceInc, Meta::VatAmount],
+                    Meta::LineType => Creator::LineType_PaymentFee,
+                ];
+                $invoice['customer']['invoice']['line'][] = $line;
+
+                // Add these amounts to the invoice totals.
+                // @see \Siel\Acumulus\Magento\Invoice\Creator\getInvoiceTotals()
+                $invoice['customer']['invoice'][Meta::InvoiceAmountInc] += $paymentInc;
+                $invoice['customer']['invoice'][Meta::InvoiceVatAmount] += $paymentVat;
+            }
         }
     }
 }
