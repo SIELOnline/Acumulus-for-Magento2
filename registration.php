b8c1d91f88b5fa877126eb3a13eaf5cff679a36f
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2020-11-28 11:06:19 +0100

    Using the function dirname() instead of __DIR__ (in registration.php) prevents errors about invalid template files when symbolic links are used.

--- registration.php
+++ registration.php
@@ -1,4 +1,6 @@
 <?php
 use \Magento\Framework\Component\ComponentRegistrar;
 
+// Using the function dirname() instead of __DIR__ prevents errors about invalid
+// template files when symbolic links are used.
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Siel_AcumulusMa2', isset($file) ? dirname($file) : __DIR__);
