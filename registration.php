017703f1951f90c3bcecd991c3b26d48ded45dd0
Erwin Derksen <erwin@burorader.com>
2021-06-29 11:28:45 +0200

    6.3.0: EU commerce threshold

--- registration.php
+++ registration.php
@@ -1,6 +1,4 @@
 <?php
 use \Magento\Framework\Component\ComponentRegistrar;
 
-// Using the function dirname() instead of __DIR__ prevents errors about invalid
-// template files when symbolic links are used.
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Siel_AcumulusMa2', isset($file) ? dirname($file) : __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Siel_AcumulusMa2', __DIR__);
