edfa66f082e8a89355ad995c976954a6b0616a1b
Erwin Derksen <erwin@burorader.com>
2023-09-28 12:30:14 +0200

    Moved test order getting and saving into our AcumulusTestUtils trait

--- tests/Magento/Integration/InvoiceCreateTest.php
+++ tests/Magento/Integration/InvoiceCreateTest.php
@@ -10,9 +10,10 @@ namespace Siel\Acumulus\Tests\Magento\Integration;
 
 use Siel\Acumulus\Fld;
 use Siel\Acumulus\Invoice\Source;
-use Siel\Acumulus\Tests\Magento\Data\TestData;
 
 use Siel\Acumulus\Tests\Magento\TestCase;
+
+use function dirname;
 
 /**
  * InvoiceCreateTest tests the process of creating an {@see Invoice}.
@@ -21,14 +22,15 @@ class InvoiceCreateTest extends TestCase
 {
     public function InvoiceDataProvider(): array
     {
+        $dataPath = dirname(__FILE__, 2) . '/Data';
         return [
-            'FR consument' => [Source::Order, 6],
-            'FR consument refund' => [Source::CreditNote, 2],
-            'FR bedrijf' => [Source::Order, 8],
-            'NL consument, winkelwagenkorting' => [Source::Order, 9],
-            'NL consument refund, winkelwagenkorting' => [Source::CreditNote, 4],
-            'FR consument, NL levering, % cataloguskorting en € winkelwagenkorting' => [Source::Order, 10],
-            'refund FR consument, NL levering, % cataloguskorting en € winkelwagenkorting' => [Source::CreditNote, 7],
+            'FR consument' => [$dataPath, Source::Order, 6],
+            'FR consument refund' => [$dataPath, Source::CreditNote, 2],
+            'FR bedrijf' => [$dataPath, Source::Order, 8],
+            'NL consument, winkelwagenkorting' => [$dataPath, Source::Order, 9],
+            'NL consument refund, winkelwagenkorting' => [$dataPath, Source::CreditNote, 4],
+            'FR consument, NL levering, % cataloguskorting en € winkelwagenkorting' => [$dataPath, Source::Order, 10],
+            'refund FR consument, NL levering, % cataloguskorting en € winkelwagenkorting' => [$dataPath, Source::CreditNote, 7],
         ];
     }
 
@@ -39,25 +41,24 @@ class InvoiceCreateTest extends TestCase
      * @dataProvider InvoiceDataProvider
      * @throws \JsonException
      */
-    public function testCreate(string $type, int $id, array $excludeFields = []): void
+    public function testCreate(string $dataPath, string $type, int $id, array $excludeFields = []): void
     {
         $invoiceSource = self::getAcumulusContainer()->createSource($type, $id);
         $invoiceAddResult = self::getAcumulusContainer()->createInvoiceAddResult('SendInvoiceTest::testCreateAndCompleteInvoice()');
         $invoice = self::getAcumulusContainer()->getInvoiceCreate()->create($invoiceSource, $invoiceAddResult);
         $result = $invoice->toArray();
-        $testData = new TestData();
         // Get order from Order{id}.json.
-        $expected = $testData->get($type, $id);
+        $expected = $this->getTestSource($dataPath, $type, $id);
         if ($expected !== null) {
             // Save order to Order{id}.latest.json, so we can compare differences ourselves.
-            $testData->save($type, $id, false, $result);
+            $this->saveTestSource($dataPath, $type, $id, false, $result);
             $this->assertCount(1, $result);
             $this->assertArrayHasKey(Fld::Customer, $result);
             $this->compareAcumulusObjects($expected[Fld::Customer], $result[Fld::Customer], Fld::Customer, $excludeFields);
         } else {
             // File does not yet exist: first time for a new test order: save order to Order{id}.json.
             // Will raise a warning that no asserts have been executed.
-            $testData->save($type, $id, true, $result);
+            $this->saveTestSource($dataPath, $type, $id, true, $result);
         }
     }
 }
